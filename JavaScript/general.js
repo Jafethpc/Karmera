655f7f38e0db72f6f0ddd948a25fc93a3acdff43
Jafethpc <hjpachecocampos@gmail.com>
2022-10-06 11:03:12 -0400

    Added CSS to Slider, with navigation.

---
+++ JavaScript/general.js
@@ -0,0 +1,18 @@
+document.querySelector(".nav-btn").addEventListener("click", function () {
+  const menu = document.querySelector(".navigation-slider");
+  const navIcon = document.querySelector(".nav-btn");
+  const invis = document.querySelector(".invisible");
+  const marginProp = window
+    .getComputedStyle(menu)
+    .getPropertyValue("margin-left");
+
+  if (marginProp === "-300px") {
+    menu.style.marginLeft = "0px";
+    navIcon.style.position = "fixed";
+    invis.style.position = "relative";
+  } else if (marginProp === "0px") {
+    menu.style.marginLeft = "-300px";
+    navIcon.style.removeProperty("position");
+    invis.style.position = "absolute";
+  }
+});
--- JavaScript/home.js
+++ JavaScript/home.js
@@ -27,22 +27,3 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = "block";
 }
-
-document.querySelector(".nav-btn").addEventListener("click", function () {
-  const menu = document.querySelector(".black");
-  const navIcon = document.querySelector(".nav-btn");
-  const invis = document.querySelector(".invisible");
-  const marginProp = window
-    .getComputedStyle(menu)
-    .getPropertyValue("margin-left");
-
-  if (marginProp === "-300px") {
-    menu.style.marginLeft = "0px";
-    navIcon.style.position = "fixed";
-    invis.style.position = "relative";
-  } else if (marginProp === "0px") {
-    menu.style.marginLeft = "-300px";
-    navIcon.style.removeProperty("position");
-    invis.style.position = "absolute";
-  }
-});
